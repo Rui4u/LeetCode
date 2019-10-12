92fd9ac422e9414387834ac0592d5596469f926a
sharui <sharui@facebank.com>
2019-10-12 17:51:09 +0800

    update：更新翻转二叉树

--- LeetCode/src/leetCode/sharui/TreeNode.java
+++ LeetCode/src/leetCode/sharui/TreeNode.java
@@ -1,5 +1,10 @@
 package leetCode.sharui;
 
-public class TreeNode {
 
-}
+ public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+ }
+
--- LeetCode/src/leetCode/sharui/_226_翻转二叉树.java
+++ LeetCode/src/leetCode/sharui/_226_翻转二叉树.java
@@ -1,5 +1,19 @@
 package leetCode.sharui;
 
 public class _226_翻转二叉树 {
-
+	public TreeNode invertTree(TreeNode root) {
+		
+		if (root == null) {
+			return root;
+		}
+		
+		TreeNode tempNode = root.left;
+		root.left = root.right;
+		root.right = tempNode;
+		
+		invertTree(root.left);
+		invertTree(root.right);
+		
+        return root;
+    }
 }
